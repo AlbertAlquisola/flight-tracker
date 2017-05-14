d036d4b0cb13c89bfd314d7bbd164779e7ee06af
AlbertAlquisola <aalquiso@gmail.com>
2017-05-14 15:17:50 -0700

    update main app and readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # flight-tracker
+
+# Run and Debug in development
+**make sure to have docker installed**
+
+# How to start flight-tracker
+1. run `docker-compose up -d` (start flight-tracker and all dependent services)
+2. run `docker exec -it flight-tracker bash` (jump inside the flight-tracker docker container)
+3. run `npm run dev-start`
+4. visit localhost:3000/errorTest (should see a predefined json error. if you do, were live!)
+
+# How to start node inspector
+1. run `docker exec -it flight-tracker bash` (jump inside the flight-tracker docker container)
+2. run `node-inspector --no-preload`
+3. visit localhost:8080 (node inspector server is running there)
+
+# Docker
+
+**building an image**
+`docker build -t ${userName}/${repoName}:${tag} -f ${fileName} .`
+
+**example**
+`docker build -t albertalquisola/flight-tracker-core:0.0.1 -f Dockerfile.core .`
+
+**how to push to dockerhub**
+`docker push ${userName}/${repoName}:${tag}`
+
+**example**
+`docker push albertalquisola/flight-tracker-core:0.0.1`
+
--- flightApp.js
+++ flightApp.js
@@ -3,27 +3,60 @@
  *
  */
 import _ from 'lodash';
-import express from 'express';
 import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
 import compression from 'compression';
-import session from 'express-session';
-import passport from 'passport';
-import Redis from 'redis';
 import ConnectRedis from 'connect-redis';
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import passport from 'passport';
+import Redis from 'ioredis';
+import expressSession from 'express-session';
+import { Strategy } from 'passport-local';
 
 import config from 'config';
 import controllers from 'controllers';
-import mw from 'middleware';
-import Logger from 'util/Logger';
 import errorHandler from 'util/errorHandler';
+import Logger from 'util/Logger';
+import mw from 'middleware';
 
-// setup mysql connection pool and initialize database
-import db from 'lib/models/database';
+// setup mysql connection pool
+import'lib/models/database';
 
-const RedisStore = ConnectRedis(session);
-const log = new Logger('flightApp.js');
 const app = express();
+const log = new Logger('flightApp.js');
+
+const redisClient = new Redis(config.redis);
+const RedisStore = ConnectRedis(expressSession);
+const session = expressSession({
+  store: new RedisStore({ client : redisClient }),
+  secret: 'takin flight',
+  resave: true,
+  saveUninitialized: true,
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(compression());
+
+// auth
+app.use(session);
+app.use(passport.initialize());
+app.use(passport.session());
+
+const passportOptions = { passReqToCallback: true };
+// passport.use('local-login', new Strategy(passportOptions, controllers.user.validateCredentials));
+// passport.use('local-signup', new Strategy(passportOptions, controllers.user.upsertUser));
+
+passport.serializeUser((user, callback) => callback(null, user));
+
+passport.deserializeUser((userInfo, callback) => {
+  controllers.user.getUserInfo(userInfo.id, (err, user) => {
+    user = _.extend({}, user, userInfo);
+
+    return callback(err, user);
+  });
+});
 
 app.get(
   '/errorTest',
@@ -40,3 +73,5 @@ app.use(errorHandler);
 app.listen(config.ports.app, () => {
   log.info('bootup', `express app listening on port ${config.ports.app}`);
 });
+
+
