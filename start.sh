cc2a3fb369f5be78b0e9dedbace29315c85b6cc5
AlbertAlquisola <aalquiso@gmail.com>
2017-09-16 22:55:13 -0700

    source prod env variables on startup

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 
 #source prod env variables
 source /root/flight-tracker/env/production;
+aws s3 cp s3://flight-tracker-bucket/production/secrets /root/flight-tracker/env/secrets --region us-east-1;
+source /root/flight-tracker/env/secrets;
 
 # in production, automatically start the app
 # docker will take care of restart policies in the event that the process or container dies
